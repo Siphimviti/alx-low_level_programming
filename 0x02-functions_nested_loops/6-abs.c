bf168f8559444cf20c9bbe9d8f014e9c91ae3c83
Siphimviti <Siphimviti@gmail.com>
2023-06-20 10:19:16 -0700

    gcc -Wall -pedantic -Werror -Wextra -std=gnu89 _putchar.c 6-main.c 6-abs.c -o 6-abs

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _abs - Computes the absolute value of an integer.
+ * @n: The int to be check.
+ * Return: Absolute value int
+ */
+
+int _abs(int n)
+
+{
+
+	if (n < 0)
+	{
+
+	return (n);
+	}
+	return (-n);
+
+}
