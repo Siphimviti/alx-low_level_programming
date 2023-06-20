fe3e09f9535ba155abf3c6217720f06212a7fec9
Siphimviti <Siphimviti@gmail.com>
2023-06-20 12:06:02 -0700

    8-24_hours.c

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -1,18 +1,23 @@
 #include "main.h"
+
 /**
  * jack_bauer - Prints every minute of the day of Jack Bauer
  * Return: ...
  */
+
 void jack_bauer(void)
+
 {
+
 int a, b, c, d;
+
 for (a = 0; a <= 2; a++)
 {
 for (b = 0; b <= 9; b++)
 {
 if ((a <= 1 && b <= 9) || (a <= 2 && b <= 3))
 {
-for (c = 0; c <= 5; c++)
+for (c = 0; d <= 9; d++)
 {
 for (d = 0; d <= 9; d++)
 {
@@ -23,8 +28,9 @@ _putchar(c + '0');
 _putchar(d + '0');
 _putchar('\n');
 }
-}	
 }
 }
 }
+}
+
 }
